178a642863ea7fca393cbd419a58500d63e46c15
BaharMahmud <bahar.a.mahmud@gmail.com>
2024-04-15 21:20:19 -0400

    added src,public,package.json,gitignore,index.htm files

---
+++ src/components/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+const About = () => {
+  return (
+    <section id="about">
+      <h2>About Me</h2>
+      <p>Write about yourself here.</p>
+      {/* Add more content as needed */}
+    </section>
+  );
+}
+export default About;
